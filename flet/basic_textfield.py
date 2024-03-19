7c3b0b16ddeb0d0f7f4d5573d869196cb2fee05a
KAnti-VP <144044786+KAnti-VP@users.noreply.github.com>
2024-03-19 13:39:44 +0100

    update

--- flet/basic_textfield.py
+++ flet/basic_textfield.py
@@ -1,17 +1,46 @@
 import flet as ft
 
 def main(page: ft.Page):
+    page.title = 'Szövegmezők'
+    page.scroll = ft.ScrollMode.AUTO
+
     def button_clicked(e):
-        t.value = f"Textboxes values are:  '{tb1.value}', '{tb2.value}', '{tb3.value}', '{tb4.value}', '{tb5.value}'."
+        t1.value = f"A szövegmezől tartalma:\n'{tb1.value}',\n '{tb2.value}',\n '{tb3.value}',\n '{tb4.value}',\n '{tb5.value}',\n '{tb6.value}',\n '{tb7.value}',\n '{tb8.value}'."
         page.update()
 
-    t = ft.Text()
+    t1 = ft.Text()
     tb1 = ft.TextField(label="Standard")
     tb2 = ft.TextField(label="Disabled", disabled=True, value="First name")
     tb3 = ft.TextField(label="Read-only", read_only=True, value="Last name")
     tb4 = ft.TextField(label="With placeholder", hint_text="Please enter text here")
     tb5 = ft.TextField(label="With an icon", icon=ft.icons.EMOJI_EMOTIONS)
-    b = ft.ElevatedButton(text="Submit", on_click=button_clicked)
-    page.add(tb1, tb2, tb3, tb4, tb5, b, t)
+    tb6 = ft.TextField(label="Multiline textbox", multiline=True, width=300, min_lines=3, max_lines=2)
+    tb7 =  ft.TextField(label="Password with reveal button", password=True, can_reveal_password=True, width=300)
+    tb8 =  ft.TextField(label="Password without reveal button", password=True, width=300)
+    b1 = ft.ElevatedButton(text="Submit", on_click=button_clicked)
+    page.add(tb1, tb2, tb3, tb4, tb5, tb6, tb7, tb8, b1, t1)
+
+    page.add(ft.Divider())
+
+    def button_clicked(e):
+        t2.value = f"Szövegmezők értékei:\n'{tb9.value}',\n '{tb10.value}',\n '{tb11.value}',\n '{tb12.value}'."
+        page.update()
+
+    t2 = ft.Text()
+    tb9 = ft.TextField(label="With prefix", prefix_text="https://")
+    tb10 = ft.TextField(label="With suffix", suffix_text=".com")
+    tb11 = ft.TextField(label="With prefix and suffix", prefix_text="https://", suffix_text=".com")
+    tb12 = ft.TextField(
+            label="My favorite color",
+            icon=ft.icons.FORMAT_SIZE,
+            hint_text="Type your favorite color",
+            helper_text="You can type only one color",
+            counter_text="0 symbols typed",
+            prefix_icon=ft.icons.COLOR_LENS,
+            suffix_text="...is your color",
+        )
+    b2 = ft.ElevatedButton(text="Submit", on_click=button_clicked)
+    page.add(tb9, tb10, tb11, tb12, b2, t2)
+
 
 ft.app(target=main)
