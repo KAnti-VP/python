25d8a6e581cac5bc4d5f4aa921266bda3c5a3ba2
KAnti-VP <144044786+KAnti-VP@users.noreply.github.com>
2024-11-12 14:00:00 +0100

    Create hazi_dolgozat.py

---
+++ tasks/string/hazi_dolgozat.py
@@ -0,0 +1,83 @@
+# Hozd létre a city változót és a felhasználótól kérd be az értékét
+# 2 pont
+
+
+# Írasd ki a következő üzenetet a city változó felhasználásával
+# "Welcome to city!"
+# 2 pont
+
+
+# Írasd ki 5-ször egymás mellé a city változót szólözzel elválasztva!
+# 2 pont
+
+
+# Kérj be egy egész és egy tizedes számot!
+# 2 pont
+
+
+# Írd ki a két szám összegét!
+# 2 pont
+
+
+# Kérj be egy új egész számot, és írasd ki a tizedes szám egész részes osztását a most megadott számmal!
+# 2 pont
+
+
+# Döntsd el, hogy az utolsó egész szám páros vagy páratlan!
+# 2 pont
+
+
+# Döntsd el, hogy melyik egész szám a nagyobb!
+# 2 pont
+
+
+# Készíts véletlen számot a két egész szám között!
+# 2 pont
+
+
+# Kerekítsd egészre a tizedes számot és írasd ki!
+# 2 pont
+
+
+# Írd ki a kerekített szám szorzótábláját!
+# 2 pont
+
+
+# Add össze a lista elemeit (két megoldás)!
+num_list = [10, 9, 11, 6, 3, 4, 5, 11, 10, 2, 5, 6, 7, 6, 9, 2]
+# 1. megoldás
+# 2 pont
+
+
+# 2. megoldás ciklussal
+# 2 pont
+
+
+# Találd meg a legkisebb és a legnagyobb elemet (két megoldás)!
+# 1. megoldás
+# 2 pont
+
+
+# 2. megoldás ciklussal
+# 2 pont
+
+
+# Rendezd a lista másolatát növekvő sorrendbe!
+# 2 pont
+
+
+# Írasd ki a rendezett lista utolsó 6 elemét!
+# 2 pont
+
+
+# Készíts a lista egyedi elemeiből egy tuple-t!
+# Egyedi elem, melyből csak egy van a listában.
+# 2 pont
+
+
+# Készíts a listából egy set-et!
+# 2 pont
+
+
+# Készíts egy listát, amely kételemű tuple-t tartalmaz (elem, darabszám)!
+# 2 pont
